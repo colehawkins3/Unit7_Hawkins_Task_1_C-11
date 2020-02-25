0b00a74de14ded8f129142f9a5187c017172d6da
colehawkins3 <stephen.hawkins808@my.tccd.edu>
2020-02-25 01:33:06 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+//Stephen Hawkins 2/24/20
+//Unit 7 Task 1: Evolution from Procedural Programming to Object Oriented Coding
+#include <iostream>
+using namespace std;
+
+int main()
+ {
+   
+return 0;
+}
